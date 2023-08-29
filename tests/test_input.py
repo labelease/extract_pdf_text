6594eba6cc43b3a91b93004397bec5a189202c3b
Emerson Rafael <emersona7x@hotmail.com>
2023-08-29 16:32:00 -0300

    [ADD] TEST USING PYTEST

---
+++ tests/test_input.py
@@ -0,0 +1,45 @@
+from pytest import mark
+
+from extract_pdf_text.extract_pdf.extract_text import Extract_PDF_Text
+
+@mark.test_input
+def test_input_pdf_success():
+
+	"""
+
+		CASE: INPUT PDF
+		WHEN: CALL MAIN FUNCTION
+		THEN: RETURN VALIDATOR TRUE
+
+	"""
+
+	# FILE
+	dir_pdf = "files_test/BO_TEXTO.pdf"
+
+	# CALL MICROSERVICE
+	result = Extract_PDF_Text().orchestra_extract_text(dir_file=dir_pdf)
+
+	# VALIDATE RESULT
+	assert result[0] == True
+
+@mark.test_input
+def test_input_txt_success():
+	"""
+
+		CASE: INPUT TXT
+		WHEN: CALL MAIN FUNCTION
+		THEN: RETURN VALIDATOR TRUE
+
+	"""
+
+	# FILE
+	dir_pdf = "files_test/news_file_i.txt"
+
+	# CALL MICROSERVICE
+	result = Extract_PDF_Text().orchestra_extract_text(dir_file=dir_pdf)
+
+	# VALIDATE RESULT
+	assert result[0] == True
+
+if __name__ == '__main__':
+	test_input_pdf_success()
